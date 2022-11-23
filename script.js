927f1c26706ab0c1b5a575b8ba9de353494d2904
Renz Landicho <renzrulandicho@gmail.com>
2022-11-23 19:25:29 +0800

    remove: console log on validateInput

--- script.js
+++ script.js
@@ -10,18 +10,15 @@ function validateInput(e) {
 
     if (e.target.value.length === 0) {
         e.target.classList.remove('validBlur');
-        console.log('empty');
     }
 
     if (e.target.checkValidity() === true && e.target.value.length > 0) {
         e.target.classList.remove('invalidBlur');
         e.target.classList.add('validBlur');
-        console.log('valid');
     }
 
     if (e.target.checkValidity() === false && e.target.value.length > 0) {
         e.target.classList.remove('validBlur');
         e.target.classList.add('invalidBlur');
-        console.log('invalid');
     }
 }
