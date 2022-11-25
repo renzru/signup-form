8feb7ee5026f8a60aea3cab28723c703be004d9f
Renz Landicho <renzrulandicho@gmail.com>
2022-11-25 12:05:35 +0800

    fix: input outline defaulting to valid color when empty

--- script.js
+++ script.js
@@ -3,17 +3,17 @@ let inputFields = document.querySelectorAll('input');
 inputFields.forEach(input => {
     input.addEventListener('focus', validateInput);
     input.addEventListener('blur', validateInput);
-    input.addEventListener('keypress', validateInput);
 });
 
 
-function validateInput(e) {
+function validateInput() {
     let inputLength = this.value.trim().length;
     let inputValid = this.checkValidity();
 
     if (inputLength === 0) {
         this.classList.remove('validBlur');
         this.classList.remove('invalidBlur');
+        this.classList.add('onFocus');
     }
     if (inputValid === true && inputLength > 0) {
         this.classList.remove('invalidBlur');
